ed4f498470a4e9342f7fb34bf85cf903b6469680
Susmarski, Paul <psusmars@nrel.gov>
2023-10-19 10:10:39 -0700

    Add some additional links for testing not just paths

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,7 +5,7 @@ function Nav() {
     <Header
       appTitle="NSRDB: National Solar Radiation Database"
       hasMobileNav
-      
+      menuCloseDelay={100}
     >
       <Menu>
         <MenuLink to="/">Home</MenuLink>
@@ -16,6 +16,10 @@ function Nav() {
         <SubMenu label="Set 2">
           <MenuLink to="/set-two/foo">Foo?</MenuLink>
           <MenuLink to="/set-two/bar">BAR?!</MenuLink>
+          <SubMenu label="About">
+            <MenuLink to="/foo/what-is-the-nsrdb">What is the NSRDB?</MenuLink>
+            <MenuLink to="/foo/announcements#section1">Announcements</MenuLink>
+          </SubMenu>
         </SubMenu>
         <MenuLink to="/data-sets">Data Sets</MenuLink>
         <MenuLink to="/resources">Resources</MenuLink>
